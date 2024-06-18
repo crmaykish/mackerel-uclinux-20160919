3bb38e30750a3ddabc3549f5f383652892a155be
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-06-17 20:19:05 -0400

    Booting kernel from 0x400 in RAM

--- flash.sh
+++ flash.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
 PORT=/dev/ttyACM0
+IMAGE_FILE="images/image.bin"
 
-echo "Copying filesystem image..."
-echo "load 300000" > $PORT
-sleep 1
-ctt -p $PORT -f images/romfs.img
+ls -lah "$IMAGE_FILE"
 
+echo "Copying Linux image..."
+echo "load" > $PORT
 sleep 1
-
-echo "Copying Linux kernel..."
-echo "load 8000" > $PORT
-sleep 1
-ctt -p $PORT -f images/image.bin
+ctt -p $PORT -f "$IMAGE_FILE"
 
 sleep 1
 echo "Booting Linux..."
