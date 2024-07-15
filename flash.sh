21ceccb8e4fa1e04f6dc39cf260c0559b48b5f29
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-07-14 21:58:13 -0400

    Flash script copies ROMFS

--- flash.sh
+++ flash.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-PORT=/dev/ttyACM0
+PORT=/dev/ttyUSB0
 IMAGE_FILE="images/image.bin"
+ROMFS_FILE="images/romfs.img"
+
+ls -lah "$ROMFS_FILE"
+
+echo "Copying ROMfs..."
+echo "load 300000" > $PORT
+sleep 1
+ctt -p $PORT -f "$ROMFS_FILE"
+
+sleep 1
 
 ls -lah "$IMAGE_FILE"
 
