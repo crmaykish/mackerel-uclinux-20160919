be0f778ab72f1d1e18e4ed880096acb4302fd1ca
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-08-03 18:05:42 -0400

    ROMfs concatenated to the kernel image, 3MB RAM installed

--- flash.sh
+++ flash.sh
@@ -4,15 +4,6 @@ PORT=/dev/ttyUSB0
 IMAGE_FILE="images/image.bin"
 ROMFS_FILE="images/romfs.img"
 
-ls -lah "$ROMFS_FILE"
-
-echo "Copying ROMfs..."
-echo "load 300000" > $PORT
-sleep 1
-ctt -p $PORT -f "$ROMFS_FILE"
-
-sleep 1
-
 ls -lah "$IMAGE_FILE"
 
 echo "Copying Linux image..."
