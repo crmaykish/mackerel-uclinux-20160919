d16bc4f0497c6cc57d9b3b6e5442742a687e2859
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-08-04 16:55:10 -0400

    Update SD creation script

--- sd.sh
+++ sd.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
+# This script creates a "bootable" SD card for Mackerel
+# First, the file size of the linux image is written to the first bytes of the SD card
+# Then, the Linux image (kernel + ROMfs) is written with a one block offset (it starts
+# at block 1, not 0)
+# The bootloader can read the first block of the SD card to determine the image size
+# and then read enough blocks to load the whole Linux image into RAM
+
 DEVICE=/dev/sdb
 
 SD_IMAGE="images/sd.img"
 
-echo "Write kernel"
-dd if=images/image.bin bs=512 count=2048 of="$SD_IMAGE"
+LINUX_SIZE=$(wc -c < images/image.bin)
 
-echo "Write romfs"
-dd conv=notrunc if=images/romfs.img obs=512 seek=2048 of="$SD_IMAGE"
+echo -n $LINUX_SIZE > size.txt
+
+echo "Write image size..."
+dd if=size.txt bs=512 count=1 of="$SD_IMAGE"
+
+echo "Write Linux image..."
+dd conv=notrunc if=images/image.bin obs=512 seek=1 of="$SD_IMAGE"
 
 echo "Flash SD card"
 dd if="$SD_IMAGE" of="$DEVICE"
+
+rm size.txt
 
 echo "Done"
