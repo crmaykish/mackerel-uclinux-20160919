b60e8f04298283e4e58f45c4bccf81b8559fc0d5
crmaykish <2431854+crmaykish@users.noreply.github.com>
2025-09-19 12:19:03 -0400

    Update motd

--- sd.sh
+++ sd.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-# This script creates a "bootable" SD card for Mackerel
+# This script creates a "bootable" SD card for Mackerel-08
 # First, the file size of the linux image is written to the first bytes of the SD card
 # Then, the Linux image (kernel + ROMfs) is written with a one block offset (it starts
 # at block 1, not 0)
 # The bootloader can read the first block of the SD card to determine the image size
 # and then read enough blocks to load the whole Linux image into RAM
 
-DEVICE=/dev/sdb
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <device>"
+    exit 1
+fi
+
+DEVICE="$1"
 
 SD_IMAGE="images/sd.img"
 
